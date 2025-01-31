316d21c7fa0ffc289f077d339f8380452b079bc3
CuriousGeorg1 <georg.m.wahlroos@gmail.com>
2025-01-31 16:04:51 +0200

    Login endpoint

--- taskline-server/src/controller/authController.ts
+++ taskline-server/src/controller/authController.ts
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { register } from "../service/authService";
+import { register, login } from "../service/authService";
 import { GetTokenRequest, RegisterRequest } from "../types";
 import { authenticated, hasSharedSecret } from "../middleware/authMiddleware";
 import { createToken } from "../service/jwtService";
@@ -7,7 +7,16 @@ import { createToken } from "../service/jwtService";
 const authController = Router();
 
 authController.post("/login", async (req, res) => {
-  //
+  const user = req.body as RegisterRequest;
+
+  try {
+    const token = await login(user.email, user.password);
+    console.log("Logged in user", user.email);
+    res.status(200).json(token);
+  } catch (e: Error | any) {
+    console.error("Error logging in user", e);
+    res.status(400).json({ message: e?.message });
+  }
 });
 
 authController.post("/register", async (req, res) => {
